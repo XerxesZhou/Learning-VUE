443702092f91610b6abb79c72de50aefc2787f2b
Xerxes zhou <33743810+XerxesZhou@users.noreply.github.com>
2019-11-13 20:57:50 +0800

    Revert "2019/11/13 1.0.2"
    This reverts commit 5f6865fa5b074b46d04c37fb69ee2d3b630cf724.

--- travel/src/pages/home/Home.vue
+++ travel/src/pages/home/Home.vue
@@ -1,17 +1,14 @@
 <template>
   <div>
     <home-header></home-header>
-    <home-Swiper></home-Swiper>
   </div>
 </template>
 <script>
     import HomeHeader from './components/Header'
-    import HomeSwiper from './components/Swiper'
     export default {
       name: "Home",
       components: {
-        HomeHeader,
-        HomeSwiper
+        HomeHeader
       }
     };
 </script> 
